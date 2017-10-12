3003ace7be9fde80af3f8b7ac2a61b861c81214f
wahyunurulhidayat <30395841+wahyunurulhidayat@users.noreply.github.com>
2017-10-12 14:15:57 +0700

    Add files via upload

---
+++ XI_RPL2_33/index.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+<meta charset="UTF-8">
+ <title>Form Pendaftaran</title>
+ <link rel="stylesheet" href="font-awesome/css/font-awesome.css">
+ <link rel="stylesheet" href="css.css">
+</head>
+<body>
+ <form action="tampil.php" method="post">
+ <h2>Instagram</h2>
+ <h3>Buat akun untuk melihat foto dan video dari teman anda</h3>
+ <div class="btnfb">
+  <a href="facebook.com"><button class="btn btn-facebook">Masuk dengan Facebook</button></a>
+ </div>
+ <h4>Atau</h3>
+ <div class="form-group">
+  <input type="text" class="input-mode" placeholder="Nomor Ponsel atau Email" name="nomor">
+ </div>
+ <div class="form-group">
+ <input type="text" class="input-mode" placeholder="Nama Lengkap" name="lengkap">
+ </div>
+ <div class="form-group">
+ <input type="text" class="input-mode" placeholder="Nama Pengguna" name="pengguna"> 
+ </div>
+ 
+  <div class="form-group">
+ <input type="password" class="input-mode" placeholder="Kata Sandi" name="sandi"> 
+ </div>
+ <br/>
+ <div class="form-group">
+  <button class="btn btn-submit" type="submit"><b>Daftar</b></i></button>
+ </div>
+ <br/>
+ <div id="ketentuan">Dengan mendaftar, Anda menyetujui <b>Ketentuan & Kebijakan Privasi</b> kami</div>
+ </form>
+
+</body>
+</html>
---
+++ XI_RPL2_33/tampil.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<p align="center"><font size="5px"><b>INFO BIODATA ANDA</b></p>
+<p align="center"><table>
+<tr><td>Nomor Ponsel atau Email</td> <td>:</td> <td><?php echo $_POST["nomor"];?></td></tr>
+<tr><td>Nama Lengkap</td> <td>:</td> <td><?php echo $_POST["lengkap"];?></td></tr>
+<tr><td>Nama Pengguna</td> <td>:</td> <td><?php echo $_POST["pengguna"];?></td></tr>
+<tr><td>Kata Sandi</td> <td>:</td> <td><?php echo $_POST["sandi"];?></td></tr>
+</table></p>
+</body>
+</html>
