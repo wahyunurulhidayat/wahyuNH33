e3b8f84e3648564f436ba5c46b43933a414c96fa
wahyunurulhidayat <30395841+wahyunurulhidayat@users.noreply.github.com>
2017-10-26 14:11:39 +0700

    Update tampil.php

--- XI_RPL2_33/tampil.php
+++ XI_RPL2_33/tampil.php
@@ -2,10 +2,21 @@
 <body>
 <p align="center"><font size="5px"><b>INFO BIODATA ANDA</b></p>
 <p align="center"><table>
-<tr><td>Nomor Ponsel atau Email</td> <td>:</td> <td><?php echo $_POST["nomor"];?></td></tr>
-<tr><td>Nama Lengkap</td> <td>:</td> <td><?php echo $_POST["lengkap"];?></td></tr>
-<tr><td>Nama Pengguna</td> <td>:</td> <td><?php echo $_POST["pengguna"];?></td></tr>
-<tr><td>Kata Sandi</td> <td>:</td> <td><?php echo $_POST["sandi"];?></td></tr>
-</table></p>
+<?php
+		$conn=mysql_connect("localhost", "root", "");
+		mysql_select_db("dbinstagram");
+		$email=$_POST['email'];
+		$nama=$_POST['nama'];
+		$username=$_POST['username'];
+		$sandi=$_POST['sandi'];
+		$sql="insert into tinstagram (email, nama, username, sandi) values ('$email', '$nama', '$username', '$sandi')";
+		mysql_query($sql);
+		echo "<table>";
+		echo "<tr><td>Nomor Ponsel atau Email</td> <td>:</td> <td>$email</td></tr>";
+		echo "<tr><td>Nama Lengkap</td> <td>:</td> <td>$nama</td></tr>";
+		echo "<tr><td>Nama Pengguna</td> <td>:</td> <td>$username</td></tr>";
+		echo "<tr><td>Kata Sandi</td> <td>:</td> <td>$sandi</td></tr>";
+		echo "</table>";
+?>
 </body>
 </html>
